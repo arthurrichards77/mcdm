efe7267ef9a0a09ac22458f0df5cc5c93f1d2f45
Arthur <arthur.richards@bristol.ac.uk>
2019-05-06 15:21:51 +0100

    Added plotting capability.

--- mcdm.py
+++ mcdm.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class Mcdm:
     """m = Mcdm(opts)
 
@@ -58,28 +60,64 @@ class Mcdm:
             new_score = sum([weights[cri]*self.get_score(opt,cri) for cri in weights.keys()])
             self.set_score(opt,name,new_score)
 
-    def weight_mixture(self):
-        num_cri = len(self.criteria())
-        for cri in self.criteria():
+    def weight_mixture(self, cri_list=None):
+        if cri_list is None:
+            cri_list=self.criteria()
+        for cri in cri_list:
+            assert cri in self.criteria(), "No such criterion: {}".format(cri)
+        num_cri = len(cri_list)
+        for cri in cri_list:
             this_name = str(cri)+"_High"
-            this_weight = {c: 1.0/(1.0+num_cri) for c in self.criteria()}
+            this_weight = {c: 1.0/(1.0+num_cri) for c in cri_list}
             this_weight[cri] = this_weight[cri]*2.0
             self.weight_criteria(this_name,this_weight)
 
+    def select_criteria(self,cri_list):
+        for cri in cri_list:
+            assert cri in self.criteria(), "No such criterion: {}".format(cri)
+        new_mcdm = Mcdm(self.options)
+        for cri in cri_list:
+            for opt in self.options:
+                new_mcdm.set_score(opt,cri,self.get_score(opt,cri))
+        return(new_mcdm)
+
+    def plot(self):
+        width=0.1
+        colors=['c','m','y','k','b','r','g']
+        fig,ax = plt.subplots()
+        for (ii,cri) in enumerate(self.criteria()):
+            cri_ind = [jj+ii*width for jj in range(len(self.options))]
+            cri_score = [self.get_score(opt,cri) for opt in self.options]
+            ax.bar(cri_ind,cri_score,width,color=colors[ii],label=str(cri))
+        ax.set_xticks([jj+0.5*ii*width for jj in range(len(self.options))])
+        ax.set_xticklabels(self.options)
+        ax.legend()
+        plt.show()
+
     def __repr__(self):
+        opt_fmt='{:8}'
+        cri_name_fmt='{:>8.8}'
+        cri_val_fmt='{:8.3f}'
         sep = ' : '
-        header = 'Option' + sep + sep.join(self.criteria()) + '\n'
-        rows = '\n'.join([str(opt) + sep + sep.join([str(self.get_score(opt,cri)) for cri in self.criteria()]) for opt in self.options])
-        return(header+rows)
+        header = opt_fmt.format('Option') + sep + sep.join([cri_name_fmt.format(cri) for cri in self.criteria()]) + '\n'
+        rows = '\n'.join([opt_fmt.format(opt) + sep + sep.join([cri_val_fmt.format(self.get_score(opt,cri)) for cri in self.criteria()]) for opt in self.options]) + '\n'
+        row_sep = '='*8 + sep + sep.join(['='*8 for cri in self.criteria()]) + '\n'
+        return(header+row_sep+rows+row_sep)
 
-travel = Mcdm(('Car','Bus','Train'))
-travel.set_score('Car','Fuel',-1.0)
-travel.set_score('Train','Price',1.0)
-print(travel)
+def travel_example():
+    travel = Mcdm(('Car','Bus','Train'))
+    travel.set_score('Car','Fuel',-1.0)
+    travel.set_score('Train','Price',1.0)
+    print(travel)
+    travel.rescale()
+    print(travel)
+    travel.weight_criteria('Intuit',{'Fuel': 1.0, 'Price': 0.1})
+    print(travel)
+    travel.weight_mixture(['Fuel','Price'])
+    print(travel)
+    print(travel.select_criteria(['Fuel_High','Price_High','Intuit']))
+    travel.select_criteria(['Fuel_High','Price_High','Intuit']).plot()
+    return(travel)
 
-travel.rescale()
-print(travel)
-
-travel.weight_mixture()
-travel.weight_criteria('Intuit',{'Fuel': 1.0, 'Price': 0.1})
-print(travel)
+if __name__=='__main__':
+    t=travel_example()
